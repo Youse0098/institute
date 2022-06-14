0308096547c71c4ff98f1a91708f4c49f1ecaa59
Youse0098 <93992241+Youse0098@users.noreply.github.com>
2022-06-14 21:47:35 +0430

    Update urls.py

--- institute/urls.py
+++ institute/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import (InstituteCreateView, InstituteDeleteView,
                     InstituteDetailView, InstituteListView,
-                    InstituteUpdateView,TodayView,MonthView,UnpaidView)
+                    InstituteUpdateView,TodayView,MonthView,UnpaidView,TotalView)
 
 app_name = "institute"
 
@@ -14,6 +14,7 @@ urlpatterns = [
     path("create/", InstituteCreateView.as_view(), name="create"),
     path("today/<int:institute_id>/", TodayView.as_view(), name="today"),
     path("month/<int:institute_id>/", MonthView.as_view(), name="month"),
-    path("unpaid/<int:institute_id>/",UnpaidView.as_view(),name="unpaid"),  
+    path("unpaid/<int:institute_id>/",UnpaidView.as_view(),name="unpaid"),
+    path("total/<int:institute_id>/",TotalView.as_view(),name="total"),
 
 ]
